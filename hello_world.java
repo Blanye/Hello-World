0948be4da71207bb1751ef50091cb6c5a29cdab4
Blanye <31044172+Blanye@users.noreply.github.com>
2017-08-15 14:08:16 -0500

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class hello_world {
+
+	public static void main(String[] args)
+	{
+		
+		int variableName = 5;
+		char character = 'c';
+		String hello = "Hello, world! ";
+
+		int x =6;
+		int y =10;
+		
+		int n = 15000;
+		int answer = 1;
+		
+		for(int i=1; i<=n; i=i+1)
+	}
+	System.out.println(answer);
+}
+
+	
